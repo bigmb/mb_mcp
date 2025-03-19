a499a29920a24942e86807a6543c081266c9eba5
Malav <malav.b93@gmail.com>
2025-03-19 02:54:31 +0000

    new update

--- install_package.py
+++ install_package.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 import os
 import subprocess
 import sys
-import argparse
 
 py_version = sys.version
 print(py_version)
@@ -16,42 +16,42 @@ print(py_requires)
 
 file = os.getcwd() 
 
-# Change to the current directory
+
+#subprocess.run(["cd",file]), check=True, stdout=subprocess.PIPE).stdout
 os.system('cd ' + file)
 
-# Update version file
-os.system('./make_version.sh 0.1.0')
+os.system('./make_version.sh')
+
 print("version file updated")
 print('*'*100)
 
-# Git operations are commented out for now
-# Uncomment if you want to use git
-# subprocess.run(["git", "pull"], check=True, stdout=subprocess.PIPE).stdout
-# print('git pull done')
-# print('*'*100)
-# subprocess.run(["git", "push"], check=True, stdout=subprocess.PIPE).stdout
-# print('*'*100)
+# subprocess.run(["git", "add", "."], check=True, stdout=subprocess.PIPE).stdout
+# subprocess.run(["git", "commit", "-am", "Bug fix commit"], check=True, stdout=subprocess.PIPE).stdout
 
+# print('git commit done')
+
+subprocess.run(["git", "pull"], check=True, stdout=subprocess.PIPE).stdout
+print('git pull done')
+print('*'*100)
+
+subprocess.run(["git", "push"], check=True, stdout=subprocess.PIPE).stdout
+subprocess.run(["git", "push", "--tags"], check=True, stdout=subprocess.PIPE).stdout
+print('*'*100)
 print('removing dist and build folders')
 
 if os.path.exists(file+'/dist'):
     os.system('sudo rm -rf '+file+'/dist')
     os.system('sudo rm -rf '+file+'/build')
-
+#subprocess.run(["ls"]),check=True, stdout=subprocess.PIPE).stdout
 os.system("ls")
 
-# Build the package
 os.system(py_requires + ' -m build')
 
 print('*'*100)
 print('wheel built')
-
-# Install the package
 print(py_requires + ' -m pip install '+file + '/dist/' +os.listdir(file +'/dist')[-1] + ' --break-system-packages')
 os.system(py_requires + ' -m pip install '+file + '/dist/' +os.listdir(file +'/dist')[-1] + ' --break-system-packages')
 
-print('package installed')
-print('*'*100)
-
-# Upload to PyPI (commented out for now)
+# print('package installed')
+# print('*'*100)
 # os.system(py_requires + ' -m twine upload dist/*')
